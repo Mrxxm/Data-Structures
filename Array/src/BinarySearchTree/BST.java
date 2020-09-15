307262555059acb41b074b7818e6324bde1edf26
Mrxxm <362190221@qq.com>
2020-09-15 17:43:33 +0800

    添加优化 查询编写

--- Array/src/BinarySearchTree/BST.java
+++ Array/src/BinarySearchTree/BST.java
@@ -27,4 +27,84 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    // 方法一
+//    public void add(E e) {
+//        if (root == null) {
+//            root = new Node(e);
+//            size++;
+//        } else {
+//            add(root, e);
+//        }
+//    }
+
+    // 方法一
+//    private void add(Node node, E e) {
+//        if (e.equals(node.e)) {
+//            return ;
+//        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+//            node.left = new Node(e);
+//            size++;
+//            return ;
+//        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+//            node.right = new Node(e);
+//            size++;
+//            return ;
+//        }
+//
+//        if (e.compareTo(node.e) < 0) {
+//            add(node.left, e);
+//        } else {
+//            add(node.right, e);
+//        }
+//    }
+
+    // 方法二
+    public void add(E e) {
+        add(root, e);
+    }
+
+    // 方法二
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+
+    // 二分搜索树的查询
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private Boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+
+        if (e.compareTo(node.e) == 0) {
+          return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            return contains(node.right, e);
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
