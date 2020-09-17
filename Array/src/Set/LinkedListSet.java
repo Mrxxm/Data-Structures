d495497e7a644bac7b8392dbedd0a5e62ff6d9d1
Mrxxm <362190221@qq.com>
2020-09-17 17:07:04 +0800

    二分搜索树实现集合 链表实现集合

---
+++ Array/src/Set/LinkedListSet.java
@@ -0,0 +1,39 @@
+package Set;
+
+
+import LinkedList.LinkedListDummy;
+
+// 线性数据结构不要求对存储元素具有可比性
+public class LinkedListSet<E> implements Set<E> {
+    private LinkedListDummy<E> list;
+
+    public LinkedListSet() {
+        list = new LinkedListDummy<E>();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return list.contains(e);
+    }
+
+    @Override
+    public void add(E e) {
+        if (!list.contains(e))
+            list.addFirst(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        list.removeElement(e);
+    }
+}
