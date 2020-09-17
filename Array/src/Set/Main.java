1e17407e18c0f44aef53e530c558dfd8ba53bc44
Mrxxm <362190221@qq.com>
2020-09-17 18:02:18 +0800

    集合测试

--- Array/src/Set/Main.java
+++ Array/src/Set/Main.java
@@ -1,7 +1,25 @@
 package Set;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(testSet(new BSTSet<Integer>()));
+        System.out.println(testSet(new LinkedListSet<Integer>()));
+    }
 
+    private static double testSet(Set<Integer> set) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+
+        int n = 100000;
+
+        for (int i = 0; i < n; i++) {
+            set.add(random.nextInt(10000));
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.00;
     }
 }
