029e261b6878dd0d0711257ef487ea3204f6e1c2
Mrxxm <362190221@qq.com>
2020-09-29 15:20:44 +0800

    堆取出元素 堆排序

--- Array/src/Heap/MaxHeap.java
+++ Array/src/Heap/MaxHeap.java
@@ -43,11 +43,46 @@ public class MaxHeap<E extends Comparable<E>> {
         data.addLast(e);
         siftUp(data.getSize() - 1);
     }
-
+    // 上浮
     private void siftUp(int index) {
         while(index > 0 && data.get(parent(index)).compareTo(data.get(index)) < 0) {
             data.swap(index, parent(index));
             index = parent(index);
+        }
+    }
+
+    public E findMax() {
+        if (data.getSize() == 0)
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+        return data.get(0);
+    }
+
+    // 取出堆中最大元素
+    public E extractMax() {
+        E ret = findMax();
+
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+
+        siftDown(0);
+
+        return ret;
+    }
+
+    // 下沉
+    private void siftDown(int index) {
+
+        while (leftChild(index) < data.getSize()) {
+            int j = leftChild(index);
+            if (j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j)) > 0) {
+                j = rightChild(index);
+                // data[j] 是 leftChild 和 rightChild 中的最大值
+            }
+            if (data.get(index).compareTo(data.get(j)) >= 0) {
+                break;
+            }
+            data.swap(index, j);
+            index = j;
         }
     }
 }
