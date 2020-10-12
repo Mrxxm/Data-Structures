807353b98c2649d4263963237b95e7983ae65354
Mrxxm <362190221@qq.com>
2020-10-12 17:50:54 +0800

    提交

--- Array/src/SegmentTree/Solutions303.java
+++ Array/src/SegmentTree/Solutions303.java
@@ -3,6 +3,7 @@ package SegmentTree;
 public class Solutions303 {
     private int[] sum;
 
+    // 构造O(n)复杂度
     public Solutions303(int[] nums) {
         sum = new int[nums.length + 1]; // sum[i]存储前i个元素和， sum[0] = 0
         sum[0] = 0;
@@ -10,6 +11,7 @@ public class Solutions303 {
             sum[i] = sum[i - 1] + nums[i - 1];
     }
 
+    // 计算O(1)复杂度
     public int sumRange(int i, int j) {
         return sum[j + 1] - sum[i];
     }
