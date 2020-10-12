783c76201bf2ffef7232216fbe4182242bacb51b
Mrxxm <362190221@qq.com>
2020-10-12 16:22:44 +0800

    线段树

---
+++ Array/src/SegmentTree/SegmentTree.java
@@ -0,0 +1,82 @@
+package SegmentTree;
+
+
+public class SegmentTree<E> {
+
+    // 满二叉树
+    private E[] tree;
+    private E[] data;
+    private Merger<E> merger;
+
+    public SegmentTree(E[] arr, Merger<E> merger) {
+
+        this.merger = merger;
+
+        data = (E[])new Object[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            data[i] = arr[i];
+        }
+
+        // 满二叉树需要的数组空间
+        tree = (E[])new Object[4 * arr.length];
+        buildSegmentTree(0, 0, data.length - 1);
+    }
+
+    // 在treeIndex的位置创建表示区间[l...r]的线段树
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        if (l == r) {
+            tree[treeIndex] = data[l];
+            return ;
+        }
+
+        // 获得左右子树的根节点
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        // 获得区间范围
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
+    public int getSize() {
+        return data.length;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= data.length)
+            throw new IllegalArgumentException("Index is illegal");
+        return data[index];
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+        ret.append('[');
+        for (int i = 0; i < tree.length; i++) {
+            ret.append(tree[i]);
+            if (i != tree.length - 1) {
+                ret.append(',');
+            }
+        }
+        ret.append(']');
+
+        return ret.toString();
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+        SegmentTree<Integer> sTree = new SegmentTree<Integer>(nums, (a, b) -> a + b);
+
+        System.out.println(sTree);
+    }
+}
